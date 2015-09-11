e6e446757956bdbcb237b3bcb1782c493e99e937
kaiqigong <cagegong@gmail.com>
2015-09-11 10:18:13 +0800

    register

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,6 +1,7 @@
 /*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
 
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import withStyles from '../../decorators/withStyles';
 import styles from './RegisterPage.css';
 import TextBox from '../TextBox'
@@ -17,15 +18,37 @@ class RegisterPage {
     this.context.onSetTitle(title);
     return (
       <div className="RegisterPage">
-        <div className="RegisterPage-container">
+        <form onSubmit={this.handleSubmit.bind(this)} className="RegisterPage-container">
           <h1>{title}</h1>
           <div>
-            <TextBox placeholder="请输入邮箱"/>
-            <TextBox placeholder="请输入密码" type="password"/>
+            <TextBox
+              placeholder="请输入邮箱"
+              ref="username" />
+            <TextBox
+              placeholder="请输入密码"
+              type="password"
+              ref="password" />
           </div>
-        </div>
+          <button className="btn">注册</button>
+        </form>
       </div>
     );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    var username = ReactDOM.findDOMNode(this.refs.username).value.trim();
+    var password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+
+    if (!username || !password) {
+      return;
+    }
+    // TODO: send request to the server
+    ReactDOM.findDOMNode(this.refs.password).value = '';
+    ReactDOM.findDOMNode(this.refs.username).value = '';
+    console.log(username, password);
+    return;
   }
 
 }
